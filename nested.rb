501029dfd5df918d900021986f9e696286dae063
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-21 18:42:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     
+      programmer_hash[:yukihiro_matsumoto] = {
+        :known_for: "Ruby",
+          :languages:
+          ["LISP", "C"]
+    }
 
 programmer_hash[:grace_hopper]
 
