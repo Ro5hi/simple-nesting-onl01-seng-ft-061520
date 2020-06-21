69d7c12fed4e03822a7c56a9a27121a9b935f4b6
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-21 19:06:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,8 +117,9 @@ def changing_alan
      
     alans_new_info = "GUI"
     programmer_hash = [:alan_kay][:known_for] = "GUI"
-    programmer_hash
+    
   end 
+  programmer_hash
 end 
 
 def adding_to_dennis
